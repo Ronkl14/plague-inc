35b7f3b3bac1018a3f01c34184abadb3b4de4104
Ronkl14 <121381849+Ronkl14@users.noreply.github.com>
2023-04-06 19:54:20 +0300

    deploying

--- server/server.js
+++ server/server.js
@@ -51,7 +51,7 @@ io.on("connection", (socket) => {
   });
 });
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
