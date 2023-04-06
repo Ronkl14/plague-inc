b9694b7f58825c33f0cf6dbf719237f395983a7e
Ronkl14 <121381849+Ronkl14@users.noreply.github.com>
2023-04-06 20:21:43 +0300

    deploying

--- client/src/pages/Lobby.js
+++ client/src/pages/Lobby.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000", { autoConnect: false });
+const socket = io("/", { autoConnect: false });
 
 function Lobby() {
   const [players, setPlayers] = useState([]);
--- server/server.js
+++ server/server.js
@@ -51,12 +51,17 @@ io.on("connection", (socket) => {
   });
 });
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-});
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Server running");
+  });
+}
 const PORT = process.env.PORT || 5000;
 
 server.listen(
