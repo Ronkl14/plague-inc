384c022636bf62e74755dcc5e7d5f9c30460a1ab
Ronkl14 <121381849+Ronkl14@users.noreply.github.com>
2023-04-18 18:04:37 +0300

    added lobby video

--- client/src/pages/Lobby.js
+++ client/src/pages/Lobby.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import socket from "../utils/socket";
 import { usePlayerGlobalContext } from "../context/PlayerContext";
@@ -7,8 +7,18 @@ function Lobby() {
   const { players } = usePlayerGlobalContext();
   const [username, setUsername] = useState("");
   const [color, setColor] = useState("red");
+  const [showReady, setShowReady] = useState(false);
 
+  const videoRef = useRef(null);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.addEventListener("loadedmetadata", () => {
+        videoRef.current.play();
+      });
+    }
+  }, []);
 
   useEffect(() => {
     if (players.length >= 2) {
@@ -30,6 +40,7 @@ function Lobby() {
   function connectHandler() {
     socket.connect();
     socket.emit("enterLobby", username, color);
+    setShowReady(true);
   }
 
   function ReadyHandler() {
@@ -37,24 +48,37 @@ function Lobby() {
   }
 
   return (
-    <div>
-      <h1>Lobby</h1>
-      <input value={username} onChange={changeUsernameHandler}></input>
-      <select name="color" onChange={colorPickHandler}>
-        <option value="red">Red</option>
-        <option value="blue">Blue</option>
-        <option value="yellow">Yellow</option>
-        <option value="purple">Purple</option>
-        <option value="orange">Orange</option>
-      </select>
-      <button onClick={connectHandler}>Connect</button>
-      {players.map((player) => (
-        <p key={player.id}>
-          {player.id}, {player.username}, {player.ready ? "yes" : "no"},
-          {player.color}
-        </p>
-      ))}
-      <button onClick={ReadyHandler}>Ready</button>
+    <div className="lobby">
+      <div className="left-lobby">
+        <h1>Plague Inc.</h1>
+        <p>Enter username:</p>
+        <input value={username} onChange={changeUsernameHandler}></input>
+        <select name="color" onChange={colorPickHandler}>
+          <option value="red">Red</option>
+          <option value="blue">Blue</option>
+          <option value="yellow">Yellow</option>
+          <option value="purple">Purple</option>
+          <option value="orange">Orange</option>
+        </select>
+        {!showReady && <button onClick={connectHandler}>Connect</button>}
+        {players.map((player) => (
+          <p key={player.id}>
+            {player.username} {player.ready ? "Ready" : ""}
+          </p>
+        ))}
+        {showReady && <button onClick={ReadyHandler}>Ready</button>}
+      </div>
+      <video
+        className="lobby-video"
+        ref={videoRef}
+        src="/assets/video/video.mp4"
+        width="600"
+        autoPlay
+        playsInline
+        controls
+        loop
+        muted
+      ></video>
     </div>
   );
 }
